d34dcc9b480c9b976f69cf916fefa579400e04e9
nishina555 <n.toshi.1.014@gmail.com>
2020-09-28 22:34:40 +0900

    create calculator mock

--- components/CalculatorApp.vue
+++ components/CalculatorApp.vue
@@ -1,5 +1,82 @@
 <template>
-  <h1>Calculator App！！！</h1>
+  <div class="container">
+    <v-text-field
+      class="result__text-field"
+      value="0"
+      label="Solo"
+      solo
+      readonly
+    ></v-text-field>
+    <v-row>
+      <v-col
+        :cols="4"
+      >
+        <v-btn depressed>
+          7
+        </v-btn>
+      </v-col>
+      <v-col
+        :cols="4"
+      >
+        <v-btn depressed>
+          8
+        </v-btn>
+      </v-col>
+      <v-col
+        :cols="4"
+      >
+        <v-btn depressed>
+          9
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        :cols="4"
+      >
+        <v-btn depressed>
+          4
+        </v-btn>
+      </v-col>
+      <v-col
+        :cols="4"
+      >
+        <v-btn depressed>
+          5
+        </v-btn>
+      </v-col>
+      <v-col
+        :cols="4"
+      >
+        <v-btn depressed>
+          6
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        :cols="4"
+      >
+        <v-btn depressed>
+          1
+        </v-btn>
+      </v-col>
+      <v-col
+        :cols="4"
+      >
+        <v-btn depressed>
+          2
+        </v-btn>
+      </v-col>
+      <v-col
+        :cols="4"
+      >
+        <v-btn depressed>
+          3
+        </v-btn>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -8,3 +85,12 @@ export default {
 
 }
 </script>
+
+<style>
+.container {
+
+}
+.result__text-field input {
+  text-align: right;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
+        <h1>Nuxt Calculator</h1>
         <calculator-app />
       </div>
     </v-col>
