25bb694e2b3dce2ee0624b5f3c0371bbcd845e49
nishina555 <n.toshi.1.014@gmail.com>
2020-09-29 11:16:30 +0900

    update

--- components/CalculatorApp.vue
+++ components/CalculatorApp.vue
@@ -1,81 +1,83 @@
 <template>
   <div class="container">
     <v-text-field
-      class="result__text-field"
+      class="calculator__result"
       value="0"
       label="Solo"
       solo
       readonly
     ></v-text-field>
-    <v-row>
-      <v-col
-        :cols="4"
-      >
-        <v-btn depressed>
-          7
-        </v-btn>
-      </v-col>
-      <v-col
-        :cols="4"
-      >
-        <v-btn depressed>
-          8
-        </v-btn>
-      </v-col>
-      <v-col
-        :cols="4"
-      >
-        <v-btn depressed>
-          9
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col
-        :cols="4"
-      >
-        <v-btn depressed>
-          4
-        </v-btn>
-      </v-col>
-      <v-col
-        :cols="4"
-      >
-        <v-btn depressed>
-          5
-        </v-btn>
-      </v-col>
-      <v-col
-        :cols="4"
-      >
-        <v-btn depressed>
-          6
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col
-        :cols="4"
-      >
-        <v-btn depressed>
-          1
-        </v-btn>
-      </v-col>
-      <v-col
-        :cols="4"
-      >
-        <v-btn depressed>
-          2
-        </v-btn>
-      </v-col>
-      <v-col
-        :cols="4"
-      >
-        <v-btn depressed>
-          3
-        </v-btn>
-      </v-col>
-    </v-row>
+    <div class="calculator__button">
+      <v-row>
+        <v-col
+          :cols="4"
+        >
+          <v-btn depressed>
+            7
+          </v-btn>
+        </v-col>
+        <v-col
+          :cols="4"
+        >
+          <v-btn depressed>
+            8
+          </v-btn>
+        </v-col>
+        <v-col
+          :cols="4"
+        >
+          <v-btn depressed>
+            9
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          :cols="4"
+        >
+          <v-btn depressed>
+            4
+          </v-btn>
+        </v-col>
+        <v-col
+          :cols="4"
+        >
+          <v-btn depressed>
+            5
+          </v-btn>
+        </v-col>
+        <v-col
+          :cols="4"
+        >
+          <v-btn depressed>
+            6
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          :cols="4"
+        >
+          <v-btn depressed>
+            1
+          </v-btn>
+        </v-col>
+        <v-col
+          :cols="4"
+        >
+          <v-btn depressed>
+            2
+          </v-btn>
+        </v-col>
+        <v-col
+          :cols="4"
+        >
+          <v-btn depressed>
+            3
+          </v-btn>
+        </v-col>
+      </v-row>
+    </div>
   </div>
 </template>
 
@@ -88,9 +90,18 @@ export default {
 
 <style>
 .container {
-
+  background: yellow;
 }
-.result__text-field input {
+
+.calculator__result {
+  background: yellowgreen;
+}
+
+.calculator__result input {
   text-align: right;
+}
+
+.calculator__button {
+  background: blue;
 }
 </style>
